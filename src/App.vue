e1f4d1c97dde424e89f89b90d601623cc5a1f4b6
MohBorLarge <shittu.mobolaji.7@gmail.com>
2022-03-10 14:38:41 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-pink-100 h-full pt-24 pl-64">
+    <HomePage msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HomePage from './components/HomePage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HomePage
+  }
+}
+</script>
+
---
+++ src/components/HomePage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="">
+    <div class="bg-white rounded-lg w-8/12">
+      <div class="flex justify-between px-8 py-4">
+        <p cl>Swap</p>
+        <button>
+          <img src="@/assets/icons/settings.png" alt="" srcset="">
+        </button>
+      </div>
+      <div class="flex justify-between px-8 py-4 bg-gray-100 h-24 mb-1">
+        <p cl>Swap</p>
+        <p>settings</p>
+      </div>
+      <div class="flex justify-between px-8 py-4 bg-gray-100 h-18">
+        <p cl>Swap</p>
+        <p>settings</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
